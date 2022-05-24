46f78a572dae240912ed50928e038ab9318b5e37
Amar3298 <79484860+Amar3298@users.noreply.github.com>
2022-05-24 12:44:58 +0530

    change in the backspace

--- index.js
+++ index.js
@@ -10,17 +10,26 @@ arr1.forEach((b1) => {
       string = "";
       document.querySelector("input").value = string;
     } else if (e.target.innerHTML == "B") {
-      a = string.slice(-1);
-      a2 = "";
-      a1 = string.replace(a, a2);
-      //   let str = string;
-      //   let arr = str.split("");
-      //   console.log(arr);
-      //   arr.pop(-1);
-      //   console.log(arr);
-      //   arr = arr.toString("");
-      //   console.log(arr);
-      document.querySelector("input").value = a1;
+      // a = string.slice(-1);
+      // console.log(a);
+      // a2 = "";
+      // a1 = string.replace(a, a2);
+      let str = string;
+      let arr = str.split("");
+      console.log(arr);
+      arr.pop(-1);
+      console.log(arr);
+      arr = arr.toString("");
+      final = "";
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] != ",") {
+          console.log(arr[i]);
+          final = final + arr[i];
+        }
+      }
+      // console.log(arr);
+      // console.log(final);
+      document.querySelector("input").value = final;
     } else {
       console.log(e.target);
       string = string + e.target.innerHTML;
